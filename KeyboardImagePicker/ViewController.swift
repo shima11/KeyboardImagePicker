afe0d8bc22fd1eedcf0833ef7c6c76631a1cf4b7
Shima <shima.jin@icloud.com>
2019-02-09 22:43:52 +0900

    update

--- KeyboardImagePicker/ViewController.swift
+++ KeyboardImagePicker/ViewController.swift
@@ -12,15 +12,80 @@ import Photos
 import RxKeyboard
 import RxSwift
 
-final class ImagePickerCarouselView: UIView {
+final class ImagePickerCarouselView: UIView, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    private let collectionView: UICollectionView
+
+    private let imageManager = PHImageManager()
+
+    private var items: [PHAsset] = []
 
     override init(frame: CGRect) {
+
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = .init(width: 80, height: 80)
+        layout.sectionInset = .init(top: 8, left: 8, bottom: 8, right: 8)
+
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.alwaysBounceHorizontal = true
+
         super.init(frame: frame)
 
-        backgroundColor = .red
+        backgroundColor = .white
+        collectionView.backgroundColor = .white
+
+        addSubview(collectionView)
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
 
         autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        setup()
     }
+
+    private func setup() {
+
+        libraryRequestAuthorization()
+
+        let result = PHAsset.fetchAssets(with: .image, options: nil)
+        result.enumerateObjects { [weak self] (obj, index, stop) in
+            self?.items.append(obj)
+        }
+
+        collectionView.reloadData()
+    }
+
+    // カメラロールへのアクセス許可
+    fileprivate func libraryRequestAuthorization() {
+        PHPhotoLibrary.requestAuthorization({ status in
+            switch status {
+            case .authorized:
+                print("authorized")
+            case .denied:
+                print("denied")
+            case .notDetermined:
+                print("NotDetermined")
+            case .restricted:
+                print("Restricted")
+            }
+        })
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+//        let item = items[indexPath.row]
+        cell.backgroundColor = .darkGray
+        return cell
+    }
+
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -189,11 +254,7 @@ class ViewController: UIViewController {
                 constraint1?.isActive = true
                 constraint2?.isActive = true
 
-//                self.view.setNeedsLayout()
-
-                UIView.animate(withDuration: 0.2, animations: {
-                    self.view.layoutIfNeeded()
-                })
+                self.view.layoutIfNeeded()
 
             })
             .disposed(by: disposeBag)
